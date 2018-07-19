1721ccf1c758ecda792580e12524ed1437775faa
sourav-kumar-fd <sourav.kumar@freshworks.com>
2018-07-20 01:53:25 +0530

    delete action added

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -35,6 +35,13 @@ class TodosController < ApplicationController
     @todos=Todo.all
   end
 
+  def destroy
+    @todo=Todo.find(params[:id])
+    @todo.destroy
+    flash[:notice]= "todo deleted successfully"
+    redirect_to todos_path
+  end
+
   def show
   	 @todo=Todo.find(params[:id])
   end
